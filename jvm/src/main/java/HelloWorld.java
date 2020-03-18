af11f90737b1355fcc2a19d554fc74b0c06768b8
tison <360548494@qq.com>
2020-03-18 11:46:42 +0800

    #012 modify some pakage

---
+++ jvm/src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @author tison
+ * @date 2020/3/18
+ * @description
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        print();
+    }
+    public static void print() {
+        String content = new String("Hello World");
+        System.out.println(content);
+    }
+}
